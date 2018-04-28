01faf408e632ec633179440281c99cd597d0afa7
unknown <ian.kwan@utexas.edu>
2018-04-28 00:23:59 -0500

    added galaxy read

--- src/TieFighter/Main.java
+++ src/TieFighter/Main.java
@@ -1,8 +1,42 @@
 package TieFighter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
     }
+
+    private WeightedGraph readGalaxy(File inputFile) throws FileNotFoundException {
+        try {
+            Scanner input = new Scanner(inputFile);
+
+            WeightedGraph weightedGraph = new WeightedGraph();
+            while(input.hasNextLine()) {
+                String line = input.nextLine();
+                String[] splitLine = line.split(" ");
+
+                Integer v = Integer.parseInt(splitLine[0]);
+                weightedGraph.addVertex(v);
+
+                for(int i = 1; i < splitLine.length; i++) {
+                    String[] weightedEdge = splitLine[i].split(",");
+                    int u = Integer.parseInt(weightedEdge[0]);
+                    double weight = Double.parseDouble(weightedEdge[1]);
+                    weightedGraph.addEdge(u, v, weight);
+                }
+            }
+
+            return weightedGraph;
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found. Please check filepath.");
+        }
+        return null;
+    }
+
+    private
 }
